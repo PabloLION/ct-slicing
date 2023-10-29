0ef49093bf3fa597a139ce285191e9a9c80514c8
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-10-29 22:45:43 +0100

    ref: raise error instead of assert

--- ct_slicing/l1_0_t_test_boxplots.py
+++ ct_slicing/l1_0_t_test_boxplots.py
@@ -24,57 +24,12 @@ from scipy import stats
 from ct_slicing.config.data_path import DATA_FOLDER, OUTPUT_FOLDER
 from ct_slicing.log import logger
 
-SAVED_PATH = DATA_FOLDER / "py-radiomics" / "slice_glcm1d.npz"
+SAVE_PATH = DATA_FOLDER / "py-radiomics" / "slice_glcm1d.npz"
 OUTPUT_DIR = OUTPUT_FOLDER / "t_test_box_plots"
 
-saved_data = np.load(SAVED_PATH, allow_pickle=True)
-
-logger.debug(f"loaded data with files {saved_data.files=} from {SAVED_PATH=}")
-slice_meta = saved_data["slice_meta"]
-slice_features = saved_data["slice_features"]  # not "slice_flat"
-# in this npz
-logger.debug(f"loaded slice_meta with {slice_meta.shape=}, {slice_meta[0]=}")
-logger.debug(f"loaded features with {slice_features.shape=}, {slice_features[0]=}")
-features_rankin_idx = saved_data["features_rankin_idx"]
-logger.debug(f"loaded features_rankin_idx with {features_rankin_idx.shape=}")
-
-print(slice_meta[0])
-list_of_slice_meta = slice_meta.tolist()
-
-
-def t_test(slice_meta, slice_features):
-    x = []
-
-    benign_indices = np.nonzero(slice_meta[:, 3] == "Benign")[0]
-    x.append(slice_features[benign_indices, :])
-
-    mali_indices = np.nonzero(slice_meta[:, 3] == "Malignant")[0]
-    x.append(slice_features[mali_indices, :])
-
-    no_nod_indices = np.nonzero(slice_meta[:, 3] == "NoNod")[0]
-    x.append(slice_features[no_nod_indices, :])
-
-    p_val = []
-
-    for i in np.arange(x[0].shape[1]):
-        aux = stats.ttest_ind(x[0][:, i], x[1][:, i])
-        p_val.append(aux[1])
-
-    p_val = np.array(p_val)
-    ranking_idx = np.argsort(p_val)
-    p_val_sort = p_val[ranking_idx]
-
-    return p_val, ranking_idx, p_val_sort
-
-
-p_val, ranking_idx, pval_sort = t_test(slice_meta, slice_features)
-
-
-# print("p_val: ", p_val)
-# print("ranking_idx: ", ranking_idx)
-# print("pval_sort: ", pval_sort)
-
-features = [
+FEATURE_COUNT = 24
+SLICE_FEATURE_NAMES = [
+    # slice_features in SAVE_PATH have 24 features in the following order
     "original_glcm_Autocorrelation",
     "original_glcm_ClusterProminence",
     "original_glcm_ClusterShade",
@@ -100,10 +55,96 @@ features = [
     "original_glcm_SumEntropy",
     "original_glcm_SumSquares",
 ]
+if len(SLICE_FEATURE_NAMES) != FEATURE_COUNT:
+    raise ValueError("SLICE_FEATURE_NAMES must have 24 features")
+
+
+def load_saved_data(save_path):
+    """
+    Load the saved `slice_meta` and `slice_features` data from `save_path`.
+    The `slice_meta` is the metadata of each slice. Its third column must be the
+    label of "Benign", "Malignant", or "NoNod".
+    The `slice_features` is the
+    features of each slice. Its shape must be (slice_num, feature_num), and
+    the feature_num must be 24 (FEATURE_COUNT). These 24 features are in the order of
+    SLICE_FEATURE_NAMES.
+    """
+    if not os.path.exists(save_path):
+        raise FileNotFoundError(f"{save_path=} does not exist")
+    saved_data = np.load(save_path, allow_pickle=True)
+    logger.debug(f"loaded data with files {saved_data.files=} from {save_path=}")
+    slice_meta = saved_data["slice_meta"]
+    slice_features = saved_data["slice_features"]  # not "slice_flat"
+    logger.debug(f"loaded slice_meta with {slice_meta.shape=}, {slice_meta[0]=}")
+    logger.debug(f"loaded features with {slice_features.shape=}, {slice_features[0]=}")
+
+    # in this npz save, there's also a features_rankin_idx, which is
+    # same as ranking_idx in t_test (asserted), but we don't need it here.
+    # features_rankin_idx = saved_data["features_rankin_idx"]
+    # logger.debug(f"loaded features_rankin_idx with {features_rankin_idx.shape=}")
+
+    if slice_meta.shape[1] != 4:
+        raise ValueError(f"{slice_meta.shape[1]=} != {4} (expected)")
+    if slice_features.shape[0] != slice_meta.shape[0]:
+        raise ValueError(
+            f"different entries from {slice_features.shape[0]=} != {slice_meta.shape[0]=}"
+        )
+    if slice_features.shape[1] != len(SLICE_FEATURE_NAMES):
+        raise ValueError(
+            f"different features from {slice_features.shape[1]=} != {len(SLICE_FEATURE_NAMES)=}"
+        )
+    return slice_meta, slice_features
+
+
+def t_test(slice_meta, slice_features):
+    """
+    This is a two sample t-test is done for each feature, between
+    benign and malignant cases. Then we can see which features have
+    different mean values between benign and malignant cases.
+
+    Args:
+        slice_meta: the metadata of each slice. Its third column must be the
+            label of "Benign", "Malignant", or "NoNod"
+        slice_features: the features of each slice. Its shape must be
+            (slice_num, feature_num), and feature_num==FEATURE_COUNT==24.
+            these 24 features are in the order of SLICE_FEATURE_NAMES
+
+    Returns:
+        p_val: the p-value of each feature in the t-test
+        ranking_idx: the ranking index of the p-value of each feature
+        pval_sort: the sorted p-value of each feature
+    """
+
+    benign_indices = np.nonzero(slice_meta[:, 3] == "Benign")[0]
+    benign_features = slice_features[benign_indices, :]
+    mali_indices = np.nonzero(slice_meta[:, 3] == "Malignant")[0]
+    malignant_features = slice_features[mali_indices, :]
+
+    # no nodules cases weren't used the in this t-test
+    # no_nod_indices = np.nonzero(slice_meta[:, 3] == "NoNod")[0]
+
+    if not (benign_features.shape[1] == malignant_features.shape[1] == FEATURE_COUNT):
+        raise ValueError("wrong args `slice_features` in t_test in t_test_box_plots")
+
+    p_val = []
+    for i in np.arange(24):
+        aux = stats.ttest_ind(benign_features[:, i], malignant_features[:, i])
+        p_val.append(aux[1])
+
+    p_val = np.array(p_val)
+    ranking_idx = np.argsort(p_val)
+    p_val_sort = p_val[ranking_idx]
+
+    return p_val, ranking_idx, p_val_sort
+
+
+# print("p_val: ", p_val)
+# print("ranking_idx: ", ranking_idx)
+# print("pval_sort: ", pval_sort)
 
 
 def save_box_plot_features(feat, y_label, feat_idx):
-    feature_name = features[feat_idx]
+    feature_name = SLICE_FEATURE_NAMES[feat_idx]
     c = feat_idx
     logger.debug(f"Plotting for {feature_name=}")
     idx_Malignant = np.nonzero(y_label == "Malignant")[0]
@@ -119,15 +160,12 @@ def save_box_plot_features(feat, y_label, feat_idx):
 
 
 if __name__ == "__main__":
-    print(slice_features.shape)
-    print(len(features))
-    print(ranking_idx.shape)
-    assert all(
-        ranking_idx == features_rankin_idx
-    ), f"{ranking_idx=} != {features_rankin_idx=}"
-    assert slice_features.shape[0] == slice_meta.shape[0]
-    assert slice_features.shape[1] == len(features)
-    for i in range(len(features)):
+    slice_meta, slice_features = load_saved_data(SAVE_PATH)
+
+    # sort the features by its t-value
+    p_val, ranking_idx, pval_sort = t_test(slice_meta, slice_features)
+
+    for i in range(len(SLICE_FEATURE_NAMES)):
         save_box_plot_features(
             feat=slice_features,
             y_label=slice_meta[:, 3],
