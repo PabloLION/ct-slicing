eaf7193b7c1ac8cd73015c8fef8b59e2b0caf958
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-11-05 06:41:37 +0100

    fix: lint and fix wrong escape in docstring

--- ct_slicing/l5_1_segmentation_validation.py
+++ ct_slicing/l5_1_segmentation_validation.py
@@ -10,95 +10,87 @@ __email__ = "debora,gtorres,csanchez,pcano@cvc.uab.es"
 __year__ = "2023"
 """
 
-### IMPORT PY LIBRARIES
-# Python Library 2 manage volumetric data
-import numpy as np
+# Unit: Segmentation Validation / Basic Validation (Segmentation)
 
-# Pyhton standard Visualization Library
+import numpy as np
 import matplotlib.pyplot as plt
-# Pyhton standard IOs Library
 import os
-# Basic Processing
+
 from skimage.filters import threshold_otsu
 from skimage import morphology as Morpho
 from scipy.ndimage import filters as filt
-
-# Validation 
 from scipy.ndimage.morphology import distance_transform_edt as bwdist
 from skimage.measure import find_contours as contour
+from ct_slicing.config.data_path import DATA_FOLDER
 
-### IMPORT SESSION FUNCTIONS
-#### PyCode Folder
-PyFolder=''
-os.chdir(PyFolder) #Change Dir 2 load session functions
-# .nii Read Data
-from NiftyIO import readNifty
-# Volume Visualization
-from VolumeCutBrowser import VolumeCutBrowser
-# Segmentation Quality Scores
-from l5_3_k_fold_sample_code import RelVolDiff,VOE,DICE,DistScores
+from vis_lib.NiftyIO import read_nifty
+from vis_lib.VolumeCutBrowser import VolumeCutBrowser
+from ct_slicing.vis_lib.segmentation_quality_scores import (
+    RelVolDiff,
+    VOE,
+    DICE,
+    DistScores,
+)
 
 
 ######## 1. LOAD DATA
 
 #### Data Folders
-SessionDataFolder='/Users/pau/Downloads/OneDrive_1_12-4-2023'#r'D:\Teaching\Master\DataSci4Health\2023_ML4PM\Week 08 - Introduction\Dataset'
+SessionDataFolder = "/Users/pau/Downloads/OneDrive_1_12-4-2023"  # r'D:\Teaching\Master\DataSci4Health\2023_ML4PM\Week 08 - Introduction\Dataset'
 os.chdir(SessionDataFolder)
 
 
-CaseFolder='CT'
-NiiFile='LIDC-IDRI-0001.nii.gz'
+IMG_PATH = DATA_FOLDER / "CT" / "image" / "LIDC-IDRI-0001.nii.gz"
+MASK_PATH = DATA_FOLDER / "CT" / "nodule_mask" / "LIDC-IDRI-0001.nii.gz"
 
 
 #### Load Intensity Volume
-NiiFile=os.path.join(SessionDataFolder,CaseFolder,'image',NiiFile)
-niiROI,_=readNifty(NiiFile)
-
-# Ground truth  mask of the lesion (niiROIGT)
-NiiFile=os.path.join(SessionDataFolder,CaseFolder,'nodule_mask',NiiFile)
-niiROIGT,_=readNifty(NiiFile)
+niiROI, _ = read_nifty(IMG_PATH)
+niiROIGT, _ = read_nifty(MASK_PATH)
 
 
 ### 2. VOLUME SEGMENTATION
 Th = threshold_otsu(niiROI)
-niiROISeg=niiROI>Th
+niiROISeg = niiROI > Th
 
 ### 3. VALIDATION SCORES
 # Axial Cut
-k=int(niiROI.shape[2]/2) # Cut at the middle of the volume. Change k to get other cuts
-SA=niiROI[:,:,k]
-SAGT=niiROIGT[:,:,k]
-SASeg=niiROISeg[:,:,k]
+k = int(
+    niiROI.shape[2] / 2
+)  # Cut at the middle of the volume. Change k to get other cuts
+SA = niiROI[:, :, k]
+SAGT = niiROIGT[:, :, k]
+SASeg = niiROISeg[:, :, k]
 
 # 3.1 Visualize GT contours over SA
-fig=plt.figure()
-plt.imshow(SA,cmap='gray')
-plt.contour(SAGT,[0.5],colors='r')
+fig = plt.figure()
+plt.imshow(SA, cmap="gray")
+plt.contour(SAGT, [0.5], colors="r")
 
 # 3.2 Volumetric Measures
-SegVOE=VOE(niiROISeg,niiROIGT)
-SegDICE=DICE(niiROISeg,niiROIGT)
-SegRelDiff=RelVolDiff(niiROISeg,niiROIGT)
+SegVOE = VOE(niiROISeg, niiROIGT)
+SegDICE = DICE(niiROISeg, niiROIGT)
+SegRelDiff = RelVolDiff(niiROISeg, niiROIGT)
 
-SegVOE_SA=VOE(SASeg,SAGT)
-SegDICE_SA=DICE(SASeg,SAGT)
-SegRelDiff_SA=RelVolDiff(niiROISeg,niiROIGT)
+SegVOE_SA = VOE(SASeg, SAGT)
+SegDICE_SA = DICE(SASeg, SAGT)
+SegRelDiff_SA = RelVolDiff(niiROISeg, niiROIGT)
 
-# 3.3 Distance Measures 
+# 3.3 Distance Measures
 # 3.3.1 Distance Map to Otsu Segmentation SA cut
-DistSegInt=bwdist(SASeg) #Distance Map inside Segmentation
-DistSegExt=bwdist(1-SASeg) #Distance Map outside Segmentation
-DistSeg=np.maximum(DistSegInt,DistSegExt) #Distance Map at all points
+DistSegInt = bwdist(SASeg)  # Distance Map inside Segmentation
+DistSegExt = bwdist(1 - SASeg)  # Distance Map outside Segmentation
+DistSeg = np.maximum(DistSegInt, DistSegExt)  # Distance Map at all points
 
 # 3.3.2 Distance from GT to Otsu Segmentation
 # GT Mask boundary points
-BorderGT=contour(SAGT,0.5)
-i=BorderGT[0][:,0].astype(int)
-j=BorderGT[0][:,1].astype(int)
+BorderGT = contour(SAGT, 0.5)
+i = BorderGT[0][:, 0].astype(int)
+j = BorderGT[0][:, 1].astype(int)
 
 # Show histogram
-fig=plt.figure()
-plt.hist(DistSeg[i,j],bins=50,edgecolor='k')
+fig = plt.figure()
+plt.hist(DistSeg[i, j], bins=50, edgecolor="k")
 
 # 3.3.3 Distance Scores
-AvgDist,MxDist=DistScores(SASeg,SAGT)
+AvgDist, MxDist = DistScores(SASeg, SAGT)
--- ct_slicing/vis_lib/segmentation_quality_scores.py
+++ ct_slicing/vis_lib/segmentation_quality_scores.py
@@ -23,74 +23,77 @@ Liver Segmentation From CT Datasets, IEEE Trans Med Imag, 28(8),2009
 import numpy as np
 from scipy.ndimage.morphology import distance_transform_edt as bwdist
 
-def DICE(Seg,GT):
+
+def DICE(Seg, GT):
     """
-    Computes dice index between segmenation Seg and 
-    ground truth mask GT:
-        dice=2 ||Seg \intersect GT||/||Seg \union GT||
+    Computes dice index between segmenation Seg and ground truth mask GT:
+    `dice=2 ||Seg \\ intersect GT|| / ||Seg \\ union GT||`
     for || . || indicating the volume
-     
+
     INPUT:
     1> Seg: Binary ndarray of segmentation
-    2> GT:  Binary ndarray of true object 
+    2> GT:  Binary ndarray of true object
     """
-    dice=np.sum(Seg[np.nonzero(GT)])*2.0 / (np.sum(Seg) + np.sum(GT))
+
+    dice = np.sum(Seg[np.nonzero(GT)]) * 2.0 / (np.sum(Seg) + np.sum(GT))
     return dice
 
-def VOE(Seg,GT):
+
+def VOE(Seg, GT):
     """
-    Computes volume overlap error (voe) between segmenation Seg and 
+    Computes volume overlap error (voe) between segmenation Seg and
     ground truth mask GT:
-        voe=1-2 ||Seg \intersect GT||/||Seg \union GT||
+        voe=1-2 ||Seg \\ intersect GT||/||Seg \\ union GT||
     for || . || indicating the volume
-    
+
     INPUT:
         1> Seg: Binary ndarray of segmentation
-        2> GT:  Binary ndarray of true object 
+        2> GT:  Binary ndarray of true object
     """
-    voe=(1-2*np.sum(Seg*GT)) / (np.sum(Seg) + np.sum(GT))
+    voe = (1 - 2 * np.sum(Seg * GT)) / (np.sum(Seg) + np.sum(GT))
     return voe
 
-def RelVolDiff(Seg,GT):
+
+def RelVolDiff(Seg, GT):
     """
-    Computes relative volume difference between segmenation Seg and 
+    Computes relative volume difference between segmenation Seg and
     ground truth mask GT:
         RelVolDiff= ||Seg - GT||/||Seg||
     for || . || indicating the volume
-    
+
     INPUT:
         1> Seg: Binary ndarray of segmentation
-        2> GT:  Binary ndarray of true object 
+        2> GT:  Binary ndarray of true object
     """
-    RelVolDiff=(np.sum(Seg)-np.sum(GT))/np.sum(Seg)
+    RelVolDiff = (np.sum(Seg) - np.sum(GT)) / np.sum(Seg)
     return RelVolDiff
 
-def DistScores(Seg,GT):
-    
+
+def DistScores(Seg, GT):
     # Distances to Segmented Volume
-    DistSegInt=bwdist(Seg)
-    DistSegExt=bwdist(1-Seg)
-    DistSeg=np.maximum(DistSegInt,DistSegExt)
+    DistSegInt = bwdist(Seg)
+    DistSegExt = bwdist(1 - Seg)
+    DistSeg = np.maximum(DistSegInt, DistSegExt)
     # Distances to GT Volume
-    DistGTInt=bwdist(GT)
-    DistGTExt=bwdist(1-GT)
-    DistGT=np.maximum(DistGTInt,DistGTExt)
-    
+    DistGTInt = bwdist(GT)
+    DistGTExt = bwdist(1 - GT)
+    DistGT = np.maximum(DistGTInt, DistGTExt)
+
     # Boundary points
-    # OBS: This way is more accurate than using 
+    # OBS: This way is more accurate than using
     # pyhton function to compute isosurface:
-    # vertices, _,_,_ = 
+    # vertices, _,_,_ =
     # measure.marching_cubes_lewiner(Seg, level=0.9)
     # index=vertices.astype(int)
     # i=index[0];j=index[1];k=index[2]
     # idxs=np.ravel_multi_index((i,j,k),Seg.shape)
-     
-    BorderSeg=((DistSegInt<1)+(DistSegInt>1))==0;
-    BorderGT=((DistGTInt<1)+(DistGTInt>1))==0;
-    
-    DistAll= np.concatenate((DistSeg[BorderGT],DistGT[BorderSeg]),axis=0)
-    
-    DistAvg=np.mean(DistAll)
-    DistMx=np.max(DistAll)
-    
-    return DistAvg,DistMx
+
+    BorderSeg = ((DistSegInt < 1) + (DistSegInt > 1)) == 0
+    BorderGT = ((DistGTInt < 1) + (DistGTInt > 1)) == 0
+
+    DistAll = np.concatenate((DistSeg[BorderGT], DistGT[BorderSeg]), axis=0)
+
+    DistAvg = np.mean(DistAll)
+    DistMx = np.max(DistAll)
+
+    return DistAvg, DistMx
