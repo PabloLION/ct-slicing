bcfaa83e5b55fbfa51316605e79db830e4adfa74
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-12-08 19:49:41 +0100

    docs: l5_1 var names and comments

--- ct_slicing/l5_1_segmentation_validation.py
+++ ct_slicing/l5_1_segmentation_validation.py
@@ -40,19 +40,19 @@ in one slice. This might cause the threshold to be inaccurate.
 | Renamed to             | Original Name | Meaning                                       |
 | ---------------------- | ------------- | --------------------------------------------- |
 | nii_otsu               | niiROISeg     | Segmentation result with otsu threshold       |
-| sagittal_cut           | SA            | Sagittal cut                                  |
-| sagittal_truth         | SAGT          | Ground truth                                  |
+| slice_cut           | SA            | slice cut                                  |
+| slice_truth         | SAGT          | Ground truth                                  |
 | slice_index            | k             | index of the slice we are focusing on         |
-| sagittal_otsu          | SAseg         | Segmentation result with otsu on sagittal cut |
+| slice_otsu          | SAseg         | Segmentation result with otsu on slice cut |
 | whole_voe              | SegVOE        | VOE of the whole volume                       |
 | whole_dice             | SegDICE       | DICE of the whole volume                      |
 | whole_rel_diff         | SegRelDiff    | RelVolDiff of the whole volume                |
-| sagittal_voe           | SegVOE_SA     | VOE of the sagittal cut                       |
-| sagittal_dice          | SegDICE_SA    | DICE of the sagittal cut                      |
-| sagittal_rel_diff      | SegRelDiff_SA | RelVolDiff of the sagittal cut                |
-| interior_sagittal_dist | DistSegInt    | Distance Map inside Segmentation (?)          |
-| exterior_sagittal_dist | DistSegExt    | Distance Map outside Segmentation (?)         |
-| otsu_sagittal_dist     | DistSeg       | Distance Map at all points (?)                |
+| slice_voe           | SegVOE_SA     | VOE of the slice cut                       |
+| slice_dice          | SegDICE_SA    | DICE of the slice cut                      |
+| slice_rel_diff      | SegRelDiff_SA | RelVolDiff of the slice cut                |
+| interior_slice_dist | DistSegInt    | Distance Map inside Segmentation (?)          |
+| exterior_slice_dist | DistSegExt    | Distance Map outside Segmentation (?)         |
+| otsu_slice_dist     | DistSeg       | Distance Map at all points (?)                |
 | borders_truth          | BorderGT      | Ground Truth Mask boundary points             |
 | border_ys              | i             | y values of border_truth                      |
 | border_xs              | j             | x values of border_truth                      |
@@ -79,64 +79,65 @@ from ct_slicing.vis_lib.segmentation_quality_scores import (
     RelVolDiff,
 )
 
-# 1. Load Intensity Volume
+""" 1. Load Intensity Volume """
 IMG_PATH = DATA_FOLDER / "CT" / "image" / "LIDC-IDRI-0001.nii.gz"
 MASK_PATH = DATA_FOLDER / "CT" / "nodule_mask" / "LIDC-IDRI-0001_R_1.nii.gz"
 
 nii_roi, _ = read_nifty(IMG_PATH)  # ROI(region of interest) from nii file
-nii_truth, _ = read_nifty(MASK_PATH)  # ground truth of ROI from nii file
+whole_truth, _ = read_nifty(MASK_PATH)  # ground truth of ROI for the whole volume
 
-
-# 2. VOLUME SEGMENTATION
+""" 2. VOLUME SEGMENTATION """
 otsu_threshold = threshold_otsu(nii_roi)
-nii_otsu = nii_roi > otsu_threshold  # segmentation result with otsu threshold
+whole_otsu = nii_roi > otsu_threshold  # segmentation result with otsu threshold
 
-# 3. VALIDATION SCORES
-# preparation: Axial Cut, here with sagittal direction
-# to fucus on only one slice of the volume, find a proper slice_index (was `k`)
+""" 3. VALIDATION SCORES """
+# To fucus on only one slice of the volume, find a proper slice_index (was `k`)
+# the slice names are with `slice_` prefix instead of `SA` or `coronal`, to
+# be more concise and consistent if we want to use other directions.
+# Here we are using the Axial Cut, AKA Short Axis direction.
 
 # find the slice with the most non-zero pixels in the ground truth
-slice_index = np.argmax(np.sum(nii_truth, axis=(0, 1)))
-# wrong: with middle cut, sagittal_truth is all 0: k = int(nii_roi.shape[2] / 2)
+slice_index = np.argmax(np.sum(whole_truth, axis=(0, 1)))
+# wrong: with middle cut, slice_truth is all 0: k = int(nii_roi.shape[2] / 2)
 # TODO: #PR1 check why it is wrong, why all 0?
 
 # get the only slice under our focus
-sagittal_cut = nii_roi[:, :, slice_index]  # the raw image
-sagittal_truth = nii_truth[:, :, slice_index]  # the ground truth
-sagittal_otsu = nii_otsu[:, :, slice_index]  # the segmentation result
+slice_cut = nii_roi[:, :, slice_index]  # the raw image
+slice_truth = whole_truth[:, :, slice_index]  # the ground truth
+slice_otsu = whole_otsu[:, :, slice_index]  # the segmentation result
 assert (  # make sure the truth is not all 0
-    np.all(sagittal_truth == 0) == False
-), f"sagittal_truth is all 0? {np.all(sagittal_truth == 0)}"
+    np.all(slice_truth == 0) == False
+), f"slice_truth is all 0? {np.all(slice_truth == 0)}"
 
 
-# 3.1 Visualize GT contours over sagittal_cuts
+# 3.1 Visualize GT contours over slice_cuts
 plt.figure()
-plt.imshow(sagittal_cut, cmap="gray")
-plt.contour(sagittal_truth, [0.5], colors="r")
+plt.imshow(slice_cut, cmap="gray")
+plt.contour(slice_truth, [0.5], colors="r")
 # will do plt.show() in 3.3.2
 
 # 3.2 Volumetric Measures
 # measures for the whole volume
-whole_voe = VOE(nii_otsu, nii_truth)
-volume_dice = DICE(nii_otsu, nii_truth)
-volume_rel_diff = RelVolDiff(nii_otsu, nii_truth)
+whole_voe = VOE(whole_otsu, whole_truth)
+volume_dice = DICE(whole_otsu, whole_truth)
+volume_rel_diff = RelVolDiff(whole_otsu, whole_truth)
 
-# measures for the sagittal_cuts cut
-sagittal_voe = VOE(sagittal_otsu, sagittal_truth)
-sagittal_dice = DICE(sagittal_otsu, sagittal_truth)
-sagittal_rel_diff = RelVolDiff(sagittal_otsu, sagittal_truth)  # #PR3: typo?
+# measures for the slice_cuts cut
+slice_voe = VOE(slice_otsu, slice_truth)
+slice_dice = DICE(slice_otsu, slice_truth)
+slice_rel_diff = RelVolDiff(slice_otsu, slice_truth)  # #PR3: typo?
 
 # 3.3 Distance Measures
-# 3.3.1 Distance Map to Otsu Segmentation sagittal_cuts cut
-interior_sagittal_dist = bwdist(sagittal_otsu)  # Distance Map inside Segmentation
-exterior_sagittal_dist = bwdist(1 - sagittal_otsu)  # Distance Map outside Segmentation
-otsu_sagittal_dist = np.maximum(
-    interior_sagittal_dist, exterior_sagittal_dist
+# 3.3.1 Distance Map to Otsu Segmentation slice_cuts cut
+interior_slice_dist = bwdist(slice_otsu)  # Distance Map inside Segmentation
+exterior_slice_dist = bwdist(1 - slice_otsu)  # Distance Map outside Segmentation
+otsu_slice_dist = np.maximum(
+    interior_slice_dist, exterior_slice_dist
 )  # Distance Map at all points
 
 # 3.3.2 Distance from GT to Otsu Segmentation
 # GT Mask boundary points
-borders_truth = find_contours(sagittal_truth, 0.5)  # find a contour of the truth
+borders_truth = find_contours(slice_truth, 0.5)  # find a contour of the truth
 assert len(borders_truth) == 2, f"len(borders_truth) = {len(borders_truth)}"
 # #TODO: #PR5: why 2 contours? seems there's another contour that we didn't use.
 border_truth, border_another, *_ = borders_truth
@@ -148,10 +149,10 @@ border_xs = border_truth[:, 1].astype(int)
 
 # Show histogram
 plt.figure()
-plt.hist(otsu_sagittal_dist[border_ys, border_xs], bins=50, edgecolor="k")
+plt.hist(otsu_slice_dist[border_ys, border_xs], bins=50, edgecolor="k")
 plt.show()
 
 # 3.3.3 Distance Scores
-average_dist, max_dist = DistScores(sagittal_otsu, sagittal_truth)
+average_dist, max_dist = DistScores(slice_otsu, slice_truth)
 print(f"Average Distance: {average_dist}")
 print(f"Max Distance: {max_dist}")
