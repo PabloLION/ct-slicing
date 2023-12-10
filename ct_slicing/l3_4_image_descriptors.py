9381d49637d0974466bafeeee1e7ae9a78166783
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-12-10 21:31:24 +0100

    feat: some exercise and refactor for l34

--- ct_slicing/l3_4_image_descriptors.py
+++ ct_slicing/l3_4_image_descriptors.py
@@ -41,7 +41,7 @@ from ct_slicing.filter_lib.gabor_filters import GaborFilterBank2D
 from ct_slicing.filter_lib.browse_gabor_filt_bank import BrowseGaborFilterBank
 from ct_slicing.ct_logger import logger
 
-LESION_LABEL = "Lesion Values"
+# Parameters
 # choose the case id and nodule id to get the path of the nodule image and mask
 IMAGE_PATH, MASK_PATH = nii_file("CT", 1, 1)
 ROI_PATH, _ = nii_file("VOI", 1, 1)
@@ -52,52 +52,57 @@ median_size = 3  # size of median filter
 filter_image = "gaussian"  # none, gaussian, median
 gabor_params = "default"  # default, non_default
 
-# Process Parameters
+# Constant and processed parameters
 nii_roi, _ = read_nifty(ROI_PATH)
 nii_mask, _ = read_nifty(MASK_PATH)
 nii_vol, _ = read_nifty(IMAGE_PATH)
+LESION_LABEL = "Lesion Values"
 
 
-### Define Use Case Image
-# #TODO: wrap this into a function
-if image_name == "filled_square":
-    # Synthetic Filled Square
-    im = np.zeros((256, 256))
-    im[64:-64, 64:-64] = 1
-    Angle = 15
-    im = ndi.rotate(im, Angle, mode="constant")  # 2D rotation
+# Define Use Case Image
+def get_im(image_name: str) -> np.ndarray:
+    if image_name == "filled_square":
+        # Synthetic Filled Square
+        im = np.zeros((256, 256))
+        im[64:-64, 64:-64] = 1
+        Angle = 15
+        im = ndi.rotate(im, Angle, mode="constant")  # 2D rotation
+    elif image_name == "square":
+        # Synthetic Square
+        im = np.zeros((256, 256))
+        im[64:-64, 64:-64] = 1
+        Angle = 15
+        im = ndi.rotate(im, Angle, mode="constant")  # 2D rotation
+        im = ndi.gaussian_gradient_magnitude(im, sigma=2)
+    elif image_name == "SA":
+        k = int(nii_vol.shape[2] / 2)  # Cut at the middle of the volume
+        im = nii_vol[:, :, k]
+    elif image_name == "SAROI":
+        k = int(nii_roi.shape[2] / 2)  # Cut at the middle of the volume
+        im = nii_roi[:, :, k]
+    elif image_name == "SA_Mask":
+        k = int(nii_mask.shape[2] / 2)  # Cut at the middle of the volume
+        im = nii_mask[:, :, k]
+    else:
+        raise ValueError("Incorrect image_name name.")
+    return im
 
-elif image_name == "square":
-    # Synthetic Square
-    im = np.zeros((256, 256))
-    im[64:-64, 64:-64] = 1
-    Angle = 15
-    im = ndi.rotate(im, Angle, mode="constant")  # 2D rotation
 
-    im = ndi.gaussian_gradient_magnitude(im, sigma=2)
+def get_img(
+    filter_image: str, im: np.ndarray, median_size: int, gaussian_sigma
+) -> np.ndarray:
+    # Image Filtering
+    if filter_image == "gaussian":
+        img = gaussian_filter(im, sigma=gaussian_sigma)
+    elif filter_image == "median":
+        img = median_filter(im, median_size)
+    else:
+        img = im
+    return img
 
 
-elif image_name == "SA":
-    k = int(nii_vol.shape[2] / 2)  # Cut at the middle of the volume
-    im = nii_vol[:, :, k]
-
-elif image_name == "SAROI":
-    k = int(nii_roi.shape[2] / 2)  # Cut at the middle of the volume
-    im = nii_roi[:, :, k]
-
-elif image_name == "SA_Mask":
-    k = int(nii_mask.shape[2] / 2)  # Cut at the middle of the volume
-    im = nii_mask[:, :, k]
-else:
-    raise ValueError("Incorrect image_name name.")
-
-# Image Filtering
-if filter_image == "gaussian":
-    img = gaussian_filter(im, sigma=gaussian_sigma)
-elif filter_image == "median":
-    img = median_filter(im, median_size)
-else:
-    img = im
+im = get_im(image_name)
+img = get_img(filter_image, im, median_size, gaussian_sigma)
 
 # Exercise 1
 for plt_idx, ex1_sig in enumerate([2, 4, 8]):
@@ -119,10 +124,10 @@ plt.show(block=DEFAULT_PLOT_BLOCK)
 plt.close()
 
 
-######## IMAGE DESCRIPTORS
+# IMAGE DESCRIPTORS
 
-### 1. GRADIENT
-## 1.1 2D Images
+# 1. GRADIENT
+# 1.1 2D Images
 # EX1. Compute gradient of 'filled_square', 'SAROI'
 #      for different filters and analyze results
 
@@ -146,15 +151,16 @@ ax.set_title("horizontal edges")
 ax = fig1.add_subplot(144)
 ax.imshow(EdgeSob, cmap="gray")
 ax.set_title("Gradient Magnitude (EdgeDetector)")
+plt.suptitle("Vertical and horizontal Sobel Edge Detector")
 plt.show(block=DEFAULT_PLOT_BLOCK)
 plt.close()
 
-## 1.2 3D Volumes
+# 1.2 3D Volumes
 # EX2. Compute the gradient of a 3D volume
 # Hint: Add the derivatives in the z-axis
 
-### 2. RIDGES, VALLEYS
-## 2.1 2D Images
+# 2. RIDGES, VALLEYS
+# 2.1 2D Images
 # EX3. Analyze results for ridges/valleys of 'square',
 #      the opposite 1-'square' and 'filled_square'
 
@@ -165,7 +171,7 @@ sxx: np.ndarray = sobel(sx, axis=1, mode="constant")
 sy = sobel(img, axis=0, mode="constant")
 syy: np.ndarray = sobel(sy, axis=0, mode="constant")
 # Laplacian (Ridge/Valley Detector)
-Lap = sxx + syy
+laplace = sxx + syy
 
 # Show Results
 fig1 = plt.figure()
@@ -178,8 +184,9 @@ ax = fig1.add_subplot(143)
 ax.imshow(syy, cmap="gray")
 ax.set_title("horizontal ridges/valleys")
 ax = fig1.add_subplot(144)
-ax.imshow(Lap, cmap="gray")
+ax.imshow(laplace, cmap="gray")
 ax.set_title("Laplacian")
+plt.suptitle("Vertical and horizontal Laplacian (Ridge/Valley Detector)")
 plt.show(block=DEFAULT_PLOT_BLOCK)
 plt.close()
 
@@ -187,16 +194,17 @@ fig1 = plt.figure()
 ax = fig1.add_subplot(131)
 ax.imshow(img, cmap="gray")
 ax = fig1.add_subplot(132)
-ax.imshow(abs(Lap) * (Lap > 0), cmap="gray")
+ax.imshow(abs(laplace) * (laplace > 0), cmap="gray")
 ax.set_title("valleys (Positive Laplacian)")
 ax = fig1.add_subplot(133)
-ax.imshow(abs(Lap) * (Lap < 0), cmap="gray")
+ax.imshow(abs(laplace) * (laplace < 0), cmap="gray")
 ax.set_title("ridges (Negative Laplacian)")
+plt.suptitle("Abs of Laplacian (Ridge/Valley Detector)")
 plt.show(block=DEFAULT_PLOT_BLOCK)
 plt.close()
 
-### 3. GABOR FILTERS
-## 3.1 2D Images
+# 3. GABOR FILTERS
+# 3.1 2D Images
 # EX4. Compare the response to each bank of Gabor filters
 #      to the edge detector and Laplacian (ridge/valley detector)
 #      of EX1 and EX3
@@ -240,33 +248,35 @@ VolumeCutBrowser(imGab2, cut_dir=CutDirection.Sagittal)
 BrowseGaborFilterBank(GaborBank2D_2, params)
 
 
-### 4. FEATURE SPACES
-## EX5: Compare Otsu binarization that only takes into account
+# 4. FEATURE SPACES
+# EX5: Compare Otsu binarization that only takes into account
 #  image intensity to using the values of intensity together with
 #  some of the local descriptors: (intensity,Laplacian)
 
-### 4.0 Data
+# 4.0 Data
+# we are using new im, new sx,sxx,sy,syy,laplace
 # SA Cut
 k = int(nii_roi.shape[2] / 2)  # Cut at the middle of the volume
-im = nii_roi[:, :, k]
-im = (im - im.min()) / (im.max() - im.min())
-imMask = nii_mask[:, :, k]
+im: np.ndarray = nii_roi[:, :, k]
+im: np.ndarray = (im - im.min()) / (im.max() - im.min())
+im_mask = nii_mask[:, :, k]
 
 # SA cut Laplacian
 sx = sobel(im, axis=1, mode="constant")
 sxx = sobel(sx, axis=1, mode="constant")
 sy = sobel(im, axis=0, mode="constant")
 syy = sobel(sy, axis=0, mode="constant")
-Lap = sxx + syy
+laplace = sxx + syy
 
-### 4.1 Intensity thresholding
-Th = threshold_otsu(im)
-imSeg = im > Th
+# 4.1 Intensity thresholding
+otsu_threshold = threshold_otsu(im)
+im_seg = im > otsu_threshold
 
 # Show Segmentation
 plt.figure()
 plt.imshow(im, cmap="gray")
-plt.contour(im, [Th], colors="red")
+plt.contour(im, [otsu_threshold], colors="red")
+plt.title("Otsu Thresholding")
 plt.show(block=DEFAULT_PLOT_BLOCK)
 plt.close()
 
@@ -274,19 +284,20 @@ plt.close()
 plt.figure()
 plt.hist(im.flatten(), edgecolor="k", bins=5)
 plt.hist(
-    im[np.nonzero(imMask)],
+    im[np.nonzero(im_mask)],
     bins=5,
     edgecolor="k",
     alpha=0.5,
     facecolor="r",
     label=LESION_LABEL,
 )
-plt.plot([Th, Th], [0, 4000], "k", lw=2, label="Otsu Threshold")
+plt.title("Intensity Histogram")
+plt.plot([otsu_threshold, otsu_threshold], [0, 4000], "k", lw=2, label="Otsu Threshold")
 plt.legend()
 plt.show(block=DEFAULT_PLOT_BLOCK)
 plt.close()
 
-### 4.2 Feature Space Partition
+# 4.2 Feature Space Partition
 # Pixel Representation in a 2D space. In the plot
 # each pixel it is assigned a x-coordinate given by its intensity
 # and a y-coordinate given by its Laplacian
@@ -297,8 +308,10 @@ plt.close()
 
 
 plt.figure()
-plt.plot(im.flatten(), Lap.flatten(), ".")
-plt.plot(im[np.nonzero(imMask)], Lap[np.nonzero(imMask)], "r.", label=LESION_LABEL)
+plt.plot(im.flatten(), laplace.flatten(), ".")
+plt.plot(
+    im[np.nonzero(im_mask)], laplace[np.nonzero(im_mask)], "r.", label=LESION_LABEL
+)
 plt.xlabel("Intensity")
 plt.ylabel("Laplacian")
 plt.title("Pixel Distribution in the Space of Values given by (Intensity,Laplacian).")
@@ -306,18 +319,20 @@ plt.legend()
 plt.show(block=DEFAULT_PLOT_BLOCK)
 plt.close()
 
-### 4.3 Kmeans Clustering
+# 4.3 Kmeans Clustering
 # EX7: Run k-means with and without normalization of the feature space
 
-Lap = (Lap - Lap.min()) / (Lap.max() - Lap.min())
-X = np.array((im.flatten(), Lap.flatten()))
-kmeans = KMeans(n_clusters=2, n_init=10)
-kmeans = kmeans.fit(X.transpose())
-labels = kmeans.predict(X.transpose())
+laplace = (laplace - laplace.min()) / (laplace.max() - laplace.min())
+X = np.array((im.flatten(), laplace.flatten()))
+k_means = KMeans(n_clusters=2, n_init=10)
+k_means = k_means.fit(X.transpose())
+labels = k_means.predict(X.transpose())
 
 plt.figure()
-plt.plot(im.flatten(), Lap.flatten(), ".")
-plt.plot(im[np.nonzero(imMask)], Lap[np.nonzero(imMask)], "r.", label=LESION_LABEL)
+plt.plot(im.flatten(), laplace.flatten(), ".")
+plt.plot(
+    im[np.nonzero(im_mask)], laplace[np.nonzero(im_mask)], "r.", label=LESION_LABEL
+)
 plt.xlabel("Intensity")
 plt.ylabel("Laplacian")
 plt.title("Pixel Distribution in the Space of Values given by (Intensity,Laplacian).")
@@ -338,25 +353,36 @@ gradient obtained using a gaussian of sig=2, 4, 8 and a median filter of Medsze=
     commented line `# Exercise 1`. We can see that the edges are more blurred
     with higher sigmas.
     The code for median_filter of different sizes is shown under the
-    commented line `# Exercise 1`. For all median sizes, the edges in the image
-    are not changed. This is because the median filter is a non-linear filter.
-    
-    
+    commented line `# Exercise 1`. For all median sizes, the images are the 
+    same. This is because the median filter is a non-linear filter.
 
 Exercise 2. Ridges and Valleys. Compute the Laplacian, Ridges (negative
 Laplacian) and Valleys (positive Laplacian) of the use images labelled 'filled_square',
 its opposite (1-'filled_square') and ‘square’. Compare results across use cases.
+    See the code under the commented line ``# EX2. Compute the gradient of a 3D
+    volume`` The plots are with super-titles `Vertical and horizontal Laplacian
+    (Ridge/Valley Detector)` and `Abs of Laplacian (Ridge/Valley Detector)`.
+    
+    We can see that the Laplacian of the square is the same as the Laplacian of
+    the filled square.
+
 Exercise 3. Gabor Filters. Compute the default Gabor filter bank and:
 a) Visualize the two banks of filters.
+
+
 b) Apply the two filter banks to Compute the Laplacian, Ridges (negative
 Laplacian) and Valleys (positive Laplacian) of the use images labelled
 'filled_square', its opposite (1-'filled_square') and ‘square’.
+
 c) Visualize responses to each filter for the two Gabor filter banks and compare
 results to the ones obtained in Ex1 and Ex2. Would you use any of the Gabor
 filters to detect edges, valleys or ridges.
+
 d) Repeat a)-c) using the alternative Gabor filter bank. What is the difference with
 the default Gabor filter bank? Would you use any of the Gabor filters to detect
 edges, valleys or ridges.
+
+
 Exercise 4. Feature Spaces. Compute Otsu thresholding for the use image
 ‘SAROI’ and visualize the binarization. Analyse the histogram of SAROI intensity
 showing the lesion values in red and discuss if there exists a threshold (vertical line)
