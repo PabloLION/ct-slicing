f5fcb3cae0e0397c99f5a6f18913aab4ae7cbadf
PabloLION <36828324+PabloLION@users.noreply.github.com>
2023-12-27 07:14:10 +0100

    ref: refactor two functions in l52

--- ct_slicing/l5_2_k_fold_sample_code.py
+++ ct_slicing/l5_2_k_fold_sample_code.py
@@ -19,6 +19,7 @@ Maybe we can try using the features of the later layers too.
 if __name__ != "__main__":
     raise ImportError(f"Script {__file__} should not be imported as a module")
 
+from typing import Iterator, cast
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.calibration import CalibratedClassifierCV
@@ -32,6 +33,13 @@ FOLDS = 10
 
 
 def load_slice_features_diagnosis_as_numpy_array() -> tuple[np.ndarray, np.ndarray]:
+    """
+    Load and filter the slice features and diagnosis from the npz file.
+
+    Returns:
+        features: 2d array of shape (7414, 24)
+        diagnoses: 1d array of shape (7414,)
+    """
     glcm_features = np.load(SLICE_FEATURES_PATH, allow_pickle=True)
     slice_features: np.ndarray = glcm_features["slice_features"]
     # slice_features is an 2d array of shape (7414, 24)
@@ -46,18 +54,30 @@ def load_slice_features_diagnosis_as_numpy_array() -> tuple[np.ndarray, np.ndarr
     return features, diagnoses
 
 
-def k_fold_cross_validation(x: np.ndarray, y: np.ndarray, kf: KFold) -> list[float]:
-    scores = []
-    for i, (train_index, test_index) in enumerate(kf.split(x)):
-        clf = SVC(probability=True, class_weight="balanced")
-        calibrated_classifier = CalibratedClassifierCV(clf, n_jobs=-1)
-        calibrated_classifier.fit(x[train_index], y[train_index])
-        scores.append(calibrated_classifier.score(x[test_index], y[test_index]))
-        logger.info(f"Fold {i} score: {scores[-1]}")
-    return scores
+def k_fold_cross_validation(x: np.ndarray, y: np.ndarray, kf: KFold) -> Iterator[float]:
+    """
+    For each fold, train a SVC and return its score on the test set.
+
+    Args:
+        x: feature vectors
+        y: diagnoses
+        kf: KFold object
+
+    Returns:
+        Iterator of scores of each fold
+    """
+    for train_indices, test_indices in kf.split(x):
+        svc = SVC(probability=True, class_weight="balanced")
+        classifier = CalibratedClassifierCV(svc, n_jobs=-1)
+        classifier.fit(x[train_indices], y[train_indices])
+        yield cast(float, classifier.score(x[test_indices], y[test_indices]))
 
 
 kf = KFold(n_splits=FOLDS)
 features, diagnoses = load_slice_features_diagnosis_as_numpy_array()
-scores = k_fold_cross_validation(features, diagnoses, kf)
+scores = list(k_fold_cross_validation(features, diagnoses, kf))
+
+# print the scores
+for i, score in enumerate(scores):
+    logger.info(f"Fold {i} score: {score}")
 logger.info(f"average score of all folds: {sum(scores) / FOLDS}")
