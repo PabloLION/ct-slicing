9ef5932f65a779c075d43be138bb92eb98dbefe3
PabloLION <36828324+PabloLION@users.noreply.github.com>
2024-01-06 12:19:32 +0100

    feat: interpret how malignancy_value and malignancy_value relate

--- ct_slicing/data_util/_script_check_data.py
+++ ct_slicing/data_util/_script_check_data.py
@@ -5,6 +5,7 @@ So we cannot remove the CT folder.
 if __name__ != "__main__":
     raise ImportError(f"Script {__file__} should not be imported as a module")
 
+from collections import defaultdict
 from ct_slicing.config.data_path import CT_FOLDER, VOI_FOLDER
 import filecmp
 from ct_slicing.data_util.metadata_access import load_all_metadata
@@ -55,6 +56,22 @@ def compare_voi_and_metadata():
     print(f"{voi_file_ids-metadata_ids=}, {metadata_ids-voi_file_ids=}")
 
 
+def malignancy_value_vs_diagnosis():
+    all_metadata = load_all_metadata()
+    malignancy_values_to_diagnosis_values = defaultdict(set)
+    for metadata in all_metadata.values():
+        diagnosis = metadata.diagnosis_value
+        malignancy = metadata.malignancy_value
+        malignancy_values_to_diagnosis_values[malignancy].add(diagnosis)
+    print(malignancy_values_to_diagnosis_values)
+    # The result {5: {1}, 4: {1}, 2: {0, 1}, 1: {0, 1}, 3: {0, 1}} shows that
+    # malignancy value 5 and 4 are always diagnosis value 1, and malignancy
+    # value 3, 2 and 1 can have diagnosis value 0 or 1. This is saying a
+    # moderately unlikely malignant nodule can be malignant (LIDC-IDRI-1001)
+    # and a Indeterminate nodule can be benign (LIDC-IDRI-0005).
+    # So just classify the nodule to (benign/malignant) would be enough.
+
+
 if __name__ == "__main__":
     # check_voi_contains_ct()
     # result: nothing from CT is equal to anything from VOI
@@ -65,7 +82,9 @@ if __name__ == "__main__":
     # VOI image not found for LIDC-IDRI-1002_R_1.nii.gz
     # All images have mask
 
-    compare_voi_and_metadata()
+    # compare_voi_and_metadata()
     # file_ids-metadata_ids=set(), metadata_ids-file_ids={(1002, 1), (1002, 2)}
     # this result shows that all VOI files are in metadata, but two metadata are not in VOI
     # so when iter over VOI files, we can just assume the metadata exists
+
+    malignancy_value_vs_diagnosis()
